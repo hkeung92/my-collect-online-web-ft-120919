ba2b4f8ff571bebebb87303d6d528992f8c26467
Ho Yin Keung <hykeung92@gmail.com>
2019-12-13 20:45:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield (array[i])
+    i += 1
+  end
+  collect
+
+end
+
+my_collect(languages) do |language| 
+language.upcase
+end
 
 
